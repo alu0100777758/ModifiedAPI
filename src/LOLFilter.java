68be1d27d58eb9717524a8c8d21bfdc95a65b64c
Eduardo Brito Sanchez <eduardo_brito27@hotmail.com>
2015-04-17 12:37:16 +0100

    Los filtros ya los pilla eclipse

--- src/LOLFilter.java
+++ src/LOLFilter.java
@@ -1,6 +1,5 @@
 
 public class LOLFilter {
-	public static 
 	public static void filter(LolTeam [] teams){
 		
 	}
---
+++ src/supportFilter.java
@@ -0,0 +1,10 @@
+/*
+ * Class that is supposed to find out if a player is a Support Type and return
+ */
+public class supportFilter {
+
+	public supportFilter() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
