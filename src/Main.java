fa0d8e4074a12ec1ab561728996735f9c938cc46
alu0100764666 <alu0100764666@ull.edu.es>
2015-04-24 13:32:45 +0200

    Equipos

--- src/Main.java
+++ src/Main.java
@@ -18,28 +18,7 @@ public class Main {
 		MatchDetail match = null;
 		ArrayList<String> ids = new ArrayList<String>();
 		ArrayList<LolTeam> teams = new ArrayList<LolTeam>();
-		ArrayList<Long> jungleItemsIDs = new ArrayList<Long>();
-		jungleItemsIDs.add((long)1039);
-		jungleItemsIDs.add((long) 3711);
-		jungleItemsIDs.add((long)3713);
-		jungleItemsIDs.add((long) 3715);
-		jungleItemsIDs.add((long)3706);
-		jungleItemsIDs.add((long)3719);
-		jungleItemsIDs.add((long)3720);
-		jungleItemsIDs.add((long)3721);
-		jungleItemsIDs.add((long)3722);
-		jungleItemsIDs.add((long)3723);
-		jungleItemsIDs.add((long)3724);
-		jungleItemsIDs.add((long)3725);
-		jungleItemsIDs.add((long)3726);
-		jungleItemsIDs.add((long)3714);
-		jungleItemsIDs.add((long)3716);
-		jungleItemsIDs.add((long)3717);
-		jungleItemsIDs.add((long)3718);
-		jungleItemsIDs.add((long)3707);
-		jungleItemsIDs.add((long)3708);
-		jungleItemsIDs.add((long)3709);
-		jungleItemsIDs.add((long)3710);
+		
 		
 //		OrderByPosition orderedTeams;
 		ids = OrderByPosition.getMatchesId("res/matchIds/matchidALL.csv");
@@ -47,25 +26,16 @@ public class Main {
 			//System.out.println("iteracion:" + i);
 			match = loadFromFile("res/serializedMatches/" + ids.get(i) + ".sav");
 			LolTeam team = OrderByPosition.ordenPositions(match, OrderByPosition.BLUE_TEAM);
-			teams.add(team);
+			if (!team.isWeirdCase())
+				teams.add(team);
 			team = OrderByPosition.ordenPositions(match, OrderByPosition.RED_TEAM);
-			teams.add(team);
-		}
-		boolean raro = true;
-		//System.out.println("teams: \n" + teams);
-		for (int i = 0; i < teams.size(); i++) {
-			raro = true;
-			for (int j = 0; j < jungleItemsIDs.size(); j++) {
-				if (!teams.get(i).isWeirdCase() && teams.get(i).getJungle().get(0).getItems().contains(jungleItemsIDs.get(j))) {
-					raro = false;
-					break;
-				}
-			}
-			if (raro && !teams.get(i).isWeirdCase())
-				System.out.println("raro" + i);		
+			if (!team.isWeirdCase())
+				teams.add(team);
 		}
 
-//		System.out.println(error);
+		toCSV csvConverter = new toCSV();
+		
+		csvConverter.teamToCsv(teams);
 	}
 	
 	static MatchDetail loadFromFile(String fileidname)throws IOException, RiotApiException, FileNotFoundException,ClassNotFoundException{
